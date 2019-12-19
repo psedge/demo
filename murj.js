fa5c4dfc9cdf22f8698240c4755296040c961fff
psedge <peter.sedgewick@gmail.com>
2019-12-19 10:52:27 +0000

    Add WebXR trial

--- murj.js
+++ murj.js
@@ -1,109 +1,98 @@
-let container;
-let camera;
-let renderer;
-let scene;
-let halted = false;
+import { VRButton } from './vrbutton.js';
 
-let columns = []
+var Murj = {
+    container: null,
+    camera: null,
+    renderer: null,
+    backgroundScene: null,
+    scene: null,
+    columns: [],
+    init: function() {
+        this.container = document.querySelector("#scene-container");
 
-function init() {
-    // Get a reference to the container element that will hold our scene
-    container = document.querySelector("#scene-container");
+        this.backgroundScene = new THREE.Scene();
+        this.backgroundScene.background = new THREE.Color(0x000000);
+        this.scene = new THREE.Scene();
 
-    // create scenes
-    backgroundScene = new THREE.Scene();
-    backgroundScene.background = new THREE.Color(0x000000);
-    scene = new THREE.Scene();
+        const fov = 60;
+        const aspect = this.container.clientWidth / this.container.clientHeight;
+        const near = 0.1;
+        const far = 1000;
 
-    // set up the options for a perspective camera
-    const fov = 60; // fov = Field Of View
-    const aspect = container.clientWidth / container.clientHeight;
-    const near = 0.1;
-    const far = 1000;
+        this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        this.camera.position.set(12, -2, -10);
 
-    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        const light = new THREE.DirectionalLight(0x5a5a5a, 10.0);
+        light.position.set(0, 10, 0);
+        this.scene.add(light);
 
-    // every object is initially created at ( 0, 0, 0 )
-    // we'll move the camera back a bit so that we can view the scene
-    camera.position.set(0, -2, -10);
+        const foreLight = new THREE.SpotLight(0xFFFFFF, 1.0);
+        foreLight.position.set(0, 5, 25);
+        foreLight.target.position.set(0, 0, 10);
+        this.scene.add(foreLight);
 
-    // Create a directional light
-    const light = new THREE.DirectionalLight(0x5a5a5a, 10.0);
-    light.position.set(0, 10, 0);
-    scene.add(light);
+        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        this.renderer.vr.enabled = true;
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+        this.renderer.autoClear = false;
+        this.renderer.gammaInput = true;
+        this.renderer.gammaOutput = true;
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setAnimationLoop(this.animate);
 
-    const foreLight = new THREE.SpotLight(0xFFFFFF, 1.0);
-    foreLight.position.set(0, 5, 25);
-    foreLight.target.position.set(0, 0, 10);
-    scene.add(foreLight);
+        this.container.appendChild(this.renderer.domElement);
+        document.body.appendChild( VRButton.createButton( this.renderer ) );
 
-    var ambientLight = new THREE.AmbientLight(0x404040); // soft white light
-
-    // create a WebGLRenderer and set its width and height
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    renderer.autoClear = false;
-    renderer.gammaInput = true;
-    renderer.gammaOutput = true;
-    renderer.setPixelRatio(window.devicePixelRatio);
-
-    // add the automatically created <canvas> element to the page
-    container.appendChild(renderer.domElement);
-
-    for (var x = -400; x < 400; x += 25) {
-        for (var z = 0; z > -200; z -= 25) {
-            if (x > -4 && x < 4) {
-                continue
+        for (var x = -400; x < 400; x += 25) {
+            for (var z = 0; z > -200; z -= 25) {
+                this.columns.push(this.column(x, 1, z))
             }
-            columns.push(column(x, 1, z))
         }
-    }
-    
-    var columnsGeo = new THREE.Geometry();
-    for (c in columns) {
-        columnsGeo.merge(columns[c].geometry, columns[c].matrix)
-    }
-    let material = new THREE.MeshStandardMaterial({color: 0x5500FF, transparent: true});
-    material.opacity = 0.5;
-    let innerMesh = new THREE.Mesh(columnsGeo, material);
-    scene.add(innerMesh)
-}
 
-let camPos = 10;
-
-setInterval(function () {
-    if (camPos < -200 ) return
-    camera.position.z = camPos;
-    camPos -= 0.05;
-}, 1)
-
-function animate() {
-    if (halted) return
-
-    requestAnimationFrame(animate);
-
-    columns.forEach(function (c, i) {
-        if (c.position.z > camPos) {
-            scene.remove(c)
-            delete columns[i];
+        var columnsGeo = new THREE.Geometry();
+        for (var c in this.columns) {
+            columnsGeo.merge(this.columns[c].geometry, this.columns[c].matrix)
         }
-    })
+        let material = new THREE.MeshStandardMaterial({color: 0x5500FF, transparent: true});
+        material.opacity = 0.5;
+        let innerMesh = new THREE.Mesh(columnsGeo, material);
+        this.scene.add(innerMesh);
+        this.start();
 
-    renderer.render(backgroundScene, camera);
-    renderer.render(scene, camera);
-}
+        return this
+    },
+    animate: function() {
+        window.murj.columns.forEach(function (c, i) {
+            if (c.position.z > window.murj.camPos) {
+                window.murj.scene.remove(c);
+                delete window.murj.columns[i];
+            }
+        });
+        window.murj.renderer.render(window.murj.backgroundScene, window.murj.camera);
+        window.murj.renderer.render(window.murj.scene, window.murj.camera);
+    },
+    column: function(x, y, z) {
+        let geometry = new THREE.BoxGeometry(15, 75, 15);
+        let material = new THREE.MeshStandardMaterial({color: 0x5500FF, transparent: true});
+        material.opacity = 0.1;
 
-function column(x, y, z) {
-    let geometry = new THREE.BoxGeometry(20, 75, 20);
-    let material = new THREE.MeshStandardMaterial({color: 0x5500FF, transparent: true});
-    material.opacity = 0.1;
+        let innerMesh = new THREE.Mesh(geometry, material);
+        innerMesh.position.set(x, y, z);
+        innerMesh.updateMatrix();
+        return innerMesh;
+    },
+    start: function() {
+        window.murj.camPos = 10;
+        setInterval(function () {
+            if (window.murj.camPos < -200 ) return
 
-    let innerMesh = new THREE.Mesh(geometry, material);
-    innerMesh.position.set(x, y, z);
-    innerMesh.updateMatrix();
-    return innerMesh;
-}
+            window.murj.camPos -= 0.05;
+            window.murj.camera.position.z = window.murj.camPos;
+            if (window.murj.camPos % 25 == 0) {
+                window.murj.camera.lookAt(new THREE.Vector3(12, -2, window.murj.camPos))
+            }
+        }, 10)
+    }
+};
 
-init();
-
-animate();
+window.murj = Murj;
